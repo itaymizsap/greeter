9d8d848af4101f21e6fa5d7189c06fa3cc96d822
itay.mizrahi <itay.mizrahi@imperva.com>
2020-12-22 11:11:45 +0200

    cleanups

--- run-build-unit-and-comp-tests-and-jacoco-reports.sh
+++ run-build-unit-and-comp-tests-and-jacoco-reports.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-mkdir -p "greeter/jacoco/unit-tests"
-mkdir -p "greeter/jacoco/component-tests"
-#mkdir -p "greeter/jacoco/merged"
-
 rm -rf "greeter/jacoco/unit-tests/"
 rm -rf "greeter/jacoco/component-tests/"
-#rm -rf "greeter/jacoco/merged/"
+rm -rf "greeter/jacoco/merged/"
 
 mkdir -p "greeter/jacoco/unit-tests"
 mkdir -p "greeter/jacoco/component-tests"
-#mkdir -p "greeter/jacoco/merged"
+mkdir -p "greeter/jacoco/merged"
 
 
-./gradlew :day-time-resolver:clean :day-time-resolver:test :day-time-resolver:installDist -console plain || exit
-./gradlew :greeter:clean :greeter:test :greeter:installDist -console plain || exit
+./gradlew :day-time-resolver:clean -console plain || exit
+./gradlew :greeter:clean -console plain || exit
 
-docker-compose -f docker-compose-with_jacoco_agent.yml up --build --force-recreate -d
+./gradlew :day-time-resolver:test :day-time-resolver:installDist -console plain || exit
+./gradlew :greeter:test :greeter:installDist -console plain || exit
+
+docker-compose -f docker-compose-with_jacoco_agent.yml up --build --force-recreate -d || exit
 
 # wait for services to become available
-sleep 20
+sleep 30
 
-curl http://localhost:8070/greet/James
+curl http://localhost:8070/greet/Gipsy.Joe || exit
 
 printf '\n'
 
@@ -48,16 +47,16 @@ java -jar jacoco/org.jacoco.cli-0.8.6-nodeps.jar report jacoco/component-tests/j
 --xml jacoco/component-tests/report.xml \
 --html jacoco/component-tests/html
 
-# shellcheck disable=SC2039
-bash <(curl -s https://codecov.io/bash) -t 276ee190-cce7-405f-99ab-8c71cf4acb89 -c -F unit-tests -f jacoco/unit-tests/report.xml
-
-# shellcheck disable=SC2039
-bash <(curl -s https://codecov.io/bash) -t 276ee190-cce7-405f-99ab-8c71cf4acb89 -c -F component-tests -f jacoco/component-tests/report.xml
-
-
 # MERGED UNIT and COMPONENT-TESTS Jacoco REPORT
-#java -jar jacoco/org.jacoco.cli-0.8.6-nodeps.jar report jacoco/unit-tests/jacoco.exec jacoco/component-tests/jacoco.exec \
-#--classfiles build/classes/java/main \
-#--sourcefiles src/main/java \
-#--xml jacoco/merged/report.xml \
-#--html jacoco/merged/html
+java -jar jacoco/org.jacoco.cli-0.8.6-nodeps.jar report jacoco/unit-tests/jacoco.exec jacoco/component-tests/jacoco.exec \
+--classfiles build/classes/java/main \
+--sourcefiles src/main/java \
+--xml jacoco/merged/report.xml \
+--html jacoco/merged/html
+
+
+# shellcheck disable=SC2039
+bash <(curl -s https://codecov.io/bash) -t 276ee190-cce7-405f-99ab-8c71cf4acb89 -F unit-tests -f jacoco/unit-tests/report.xml
+
+# shellcheck disable=SC2039
+bash <(curl -s https://codecov.io/bash) -t 276ee190-cce7-405f-99ab-8c71cf4acb89 -F component-tests -f jacoco/component-tests/report.xml
